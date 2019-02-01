5391d505a7b7e9c7895e9e55a1d7e6580770dc70
Dylan Chamoulaud <dylan3396@hotmail.fr>
2019-02-01 15:03:16 +0100

    feature US015 :   - Home page   - Map as SVG   - Interactive map with mountain selection

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,35 +1,97 @@
-import React, { Component } from 'react';
-import logo from '../assets/images/logo.svg';
-import '../assets/css/Home.css';
-import getName from "../redux/selectors/app/getName";
-import {connect} from "react-redux";
-import {mapStateToProps} from "../helpers/redux_helpers";
+import React, {Component} from "react";
+import axios from "axios";
+import logo from "../assets/images/logo.png";
+import map from "../assets/images/map.svg";
+import "../assets/css/Home.css";
 
 class Home extends Component {
-  render() {
-  	const {name} = this.props;
+	state = {
+		currentStation: undefined,
+		stations: undefined,
+	};
 
-    return (
-      <div className="Home">
-        <header className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <p>
-            Edit <code>src/Home.js</code> and save to reload.
-          </p>
-          <a
-            className="Home-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {name}
-          </a>
-        </header>
-      </div>
-    );
-  }
+	componentDidMount() {
+		console.log("componentDidMount");
+		this.setUpMap();
+	}
+
+	render() {
+		console.log("render");
+		const {stations, currentStation} = this.state;
+
+		return (
+			<div className="home">
+				<header>
+					<nav className="bg-primary color-white">
+						<div className="logo item">
+							<img src={logo} alt="l'oeil du bouquetin"/>
+						</div>
+						<div className="item">
+							<a href="/" className="color-white">
+								<h1 className="brand">L'oeil du bouquetin</h1>
+							</a>
+						</div>
+					</nav>
+				</header>
+				<div className="container">
+					<div className="titles">
+						<h2>Carte intéractive</h2>
+						{currentStation && <h2>{currentStation}</h2>}
+					</div>
+					<div className="left">
+						<object data={map} className="map" type="text/html"/>
+					</div>
+					<div className="right">
+						{stations && stations.map(station => <table key={station.id}>
+							station.name
+						</table>)}
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	resetStations = () => {
+		console.log("resetStations");
+		this.setState({stations: null, currentStation: null});
+		for(let mountain of this.mountains){
+			mountain.style.fill = null;
+		}
+	};
+
+	fetchStations = (e) => {
+		console.log("fetchStations");
+		this.resetStations();
+		const mountain             = e.currentTarget.id;
+		e.currentTarget.style.fill = "#ac3737";
+		const mountains            = {
+			corse: "Corse",
+			med: "Méditerranée",
+			centre: "Massif central",
+			alpesud: "Alpes du sud",
+			alpenord: "Alpes du nord",
+			jura: "Jura",
+			vosges: "Vosges",
+		};
+		this.setState({
+			currentStation: mountains[mountain],
+		});
+	};
+
+	setUpMap = () => {
+		console.log("setUpMap");
+		const map  = document.body.querySelector(".map");
+		map.onload = () => {
+			const mountains = map.contentDocument.querySelectorAll("#layer101 > path");
+			this.mountains   = mountains;
+			for (let mountain of mountains) {
+				mountain.style.cursor = "pointer";
+				mountain.addEventListener("mouseenter", function () {if (this.style.fill !== "rgb(172, 55, 55)") this.style.fill = "#FF5252";});
+				mountain.addEventListener("mouseleave", function () {if (this.style.fill !== "rgb(172, 55, 55)") this.style.fill = null;});
+				mountain.addEventListener("click", this.fetchStations);
+			}
+		};
+	};
 }
 
-export default connect(mapStateToProps({
-	name: getName
-}))(Home);
+export default Home;
