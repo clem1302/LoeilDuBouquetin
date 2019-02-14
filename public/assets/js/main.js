3a48aebc62e6a359556b561b442e327f185a0d9e
Dylan Chamoulaud <dylan3396@hotmail.fr>
2019-02-14 11:31:25 +0100

    Easter Egg (Konami Code)

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,17 +1,16 @@
-var egg = new Egg("up,up,down,down,left,right,left,right,b,a", function() {
-	const image = new Image();
-	image.src = "https://media1.tenor.com/images/1c206f81e946ce9532351d1c49bf730a/tenor.gif?itemid=5255913";
-	image.style.position= "fixed";
-	image.style.top = "50%";
-	image.style.left = "50%";
-	image.style.transform = "translate(-50%, -50%)";
-	image.style.zIndex = 9999999;
-	image.onload = function(){
-		document.body.appendChild(image);
-		setTimeout(function(){
-			document.body.removeChild(image);
-		},3000)
-	}
+const image           = new Image();
+image.src             = "https://media1.tenor.com/images/1c206f81e946ce9532351d1c49bf730a/tenor.gif?itemid=5255913";
+image.style.position  = "fixed";
+image.style.top       = "50%";
+image.style.left      = "50%";
+image.style.transform = "translate(-50%, -50%)";
+image.style.zIndex    = 9999999;
 
+var egg = new Egg("up,up,down,down,left,right,left,right,b,a", function () {
+
+	document.body.appendChild(image);
+	setTimeout(function () {
+		document.body.removeChild(image);
+	}, 3000);
 
 }).listen();
