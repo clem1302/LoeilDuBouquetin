c87dc0365ce6e15009c7fb44905954e74f711870
BOISSE Maxime <m.boisse@groupeonepoint.com>
2019-03-06 16:25:25 +0100

    fix Home Test path

--- src/tests/screens/Home.test.js
+++ src/tests/screens/Home.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
-import Home from "../screens/Home";
+import Home from "../../screens/Home";
 import axios from "axios";
 import sinon from 'sinon';
 
