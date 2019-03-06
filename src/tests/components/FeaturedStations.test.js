ff2c6b5df954fc85d8740735134c1adaec94a835
SombreroElGringo <florent.pailhes@hotmail.fr>
2019-03-06 20:38:51 +0100

    Update FeaturedStations.test.js

--- src/tests/components/FeaturedStations.test.js
+++ src/tests/components/FeaturedStations.test.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { mount, shallow } from "enzyme";
-import { BrowserRouter as Router } from "react-router-dom";
+import { shallow } from "enzyme";
 import FeaturedStations from "../../components/FeaturedStations";
-import renderer from "react-test-renderer";
 
 const stationsMock = [
   {
